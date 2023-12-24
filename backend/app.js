ad89312ef9cd38e5c21eb302f944b7097f85e5f2
Armand Allakhverdyan <armand.all@mail.ru>
2023-12-24 11:19:55 +0700

    mesto

--- backend/app.js
+++ backend/app.js
@@ -31,6 +31,12 @@ app.use(express.json());
 
 app.use(requestLogger);
 
+app.get('/crash-test', () => {
+  setTimeout(() => {
+    throw new Error('Сервер сейчас упадёт');
+  }, 0);
+});
+
 app.post('/signup', celebrate({
   body: Joi.object().keys({
     name: Joi.string().min(2).max(30),

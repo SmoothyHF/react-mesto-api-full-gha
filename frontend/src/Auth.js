592eb737df8d313e7cf6013bb315ef1a3c25bb3d
Armand Allakhverdyan <armand.all@mail.ru>
2023-12-24 15:52:21 +0700

    mesto

--- frontend/src/Auth.js
+++ frontend/src/Auth.js
@@ -2,7 +2,7 @@ import checkResponse from "./utils/checkResponse";
 
 // export const BASE_URL = 'https://auth.nomoreparties.co';
 // export const BASE_URL = 'http://localhost:3000';
-export const BASE_URL = 'http://mesto-smoothy.nomoredomainsmonster.ru/api';
+export const BASE_URL = 'http://mesto-smoothy.nomoredomainsmonster.ru';
 
 export function register(email, password) {
     return fetch(`${BASE_URL}/signup/`, {
